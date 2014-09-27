8ddefeae0b987fb87781849cfdefd65bbb9a79e8
Edward Price <ted.price@gmail.com>
2014-09-27 00:18:37 -0500

    Updating tests for splice

--- btree.go
+++ btree.go
@@ -1,5 +1,11 @@
 package btree
 
+import _ "fmt"
+
+var (
+	degree = 2
+)
+
 type BTree struct {
 	degree int
 	root   Node
@@ -14,7 +20,7 @@ func (b BTree) insert(key int, value string) {
 		oldRoot := b.root
 		b.root = Node{degree: b.degree}
 		b.root.children = append(b.root.children, oldRoot)
-		b.root.split(0)
+		//b.root.split(0)
 	}
 
 	b.root.insert(key, value)
@@ -31,9 +37,25 @@ func (n Node) search(key int) []int {
 	return []int{}
 }
 
-func (n Node) split(key int) []int {
-	return []int{}
+/*
+func (n Node) split(i int) {
+	splitChild := n.children[i]
+	newChild := new(Node)
+	middle := degree - 1
+
+	newChild.keys = splitChild.keys.splice(middle + 1)
+	newChild.values = splitChild.values.splice(middle + 1)
+	newChild.children = splitChild.children.splice(middle + 1)
+
+	n.children.splice(i+1, 0, newChild)
+	n.keys.splice(i, 0, splitChild.keys[middle])
+	n.values.splice(i, 0, splitChild.values[middle])
+
+	splitChild.keys.splice(middle)
+	splitChild.values.splice(middle)
 }
+
+*/
 
 func (n Node) insert(key int, value string) {
 	var i int
@@ -45,31 +67,77 @@ func (n Node) insert(key int, value string) {
 	}
 
 	if n.isLeaf() {
-		splice(n.keys, i, 0, key)
-		splice(n.values, i, 0, key)
+		//splice(n.keys, i, 0, key)
+		//splice(n.values, i, 0, key)
 	} else {
 		child := n.children[i]
 
 		if child.isFull() {
-			n.split(i)
+			//			n.split(i)
 		}
 
 		child.insert(key, value)
 	}
 }
 
-type Interface interface{}
+type Indexable interface {
+	value()
+}
+
+type MyInt int
+
+func (m MyInt) value() {
+	//return int(m)
+}
+
+type MyString string
+
+func (m MyString) value() {
+	//return int(m)
+}
+
+type IndexableMap map[Indexable]string
+type IndexableList []Indexable
+
+// Insert inserts the value into the slice at the specified index,
+// which must be in range.
+// The slice must have room for the new element.
+func Insert(slice []int, index, value int) []int {
+	// Grow the slice by one element.
+	slice = slice[0 : len(slice)+1]
+	// Use copy to move the upper part of the slice out of the way and open a hole.
+	copy(slice[index+1:], slice[index:])
+	// Store the new value.
+	slice[index] = value
+	// Return the result.
+	return slice
+}
 
 // Splice changes content of an array
-// :array is array to change
 // :startIndex, where to begin changes
 // :howMany, old array elements to remove
 // :... elements to add
-func splice(array []Interface, startIndex int, howMany int, elements Interface) []Interface {
-	startOfArray := array[:startIndex]
-	startOfArray = append(startOfArray, elements)
-	endOfArray := array[startIndex+howMany:]
-	return append(startOfArray, endOfArray)
+func (il IndexableList) Splice(startIndex int, howMany int, elements ...Indexable) IndexableList {
+
+	if howMany == 0 {
+		return il
+	}
+
+	length := len(il) + len(elements) - howMany
+	slice := make(IndexableList, length)
+
+	copy(slice[:startIndex], il[:startIndex])
+
+	var numNewElements int
+	for i, element := range elements {
+		if element != nil {
+			numNewElements++
+			slice[startIndex+i] = element
+		}
+	}
+	copy(slice[startIndex+numNewElements:], il[(startIndex+howMany):])
+
+	return slice
 }
 
 func (n Node) isLeaf() bool {
--- btree_test.go
+++ btree_test.go
@@ -1,6 +1,7 @@
 package btree
 
 import (
+	_ "fmt"
 	"testing"
 )
 
@@ -23,5 +24,53 @@ func TestNodeIsFull(t *testing.T) {
 func TestSearchRoot(t *testing.T) {
 }
 
+func equals(a, b IndexableList) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestSplice(t *testing.T) {
+
+	vals := IndexableList{MyInt(1), MyInt(2), MyInt(3), MyInt(4)}
+
+	var tests = []struct {
+		exp IndexableList
+		res func() IndexableList
+	}{
+		{
+			// Remove nothing
+			vals,
+			func() IndexableList { return vals.Splice(0, 0) },
+		},
+		{
+			// Remove 1
+			IndexableList{MyInt(1), MyInt(2), MyInt(4)},
+			func() IndexableList { return vals.Splice(2, 1) },
+		},
+		{
+			// Remove 2
+			IndexableList{MyInt(1), MyInt(4)},
+			func() IndexableList { return vals.Splice(1, 2) },
+		},
+		{
+			// Remove 2, Add 3
+			IndexableList{MyInt(1), MyInt(101), MyInt(13), MyInt(14), MyInt(4)},
+			func() IndexableList { return vals.Splice(1, 2, MyInt(101), MyInt(13), MyInt(14)) },
+		},
+	}
+
+	for _, test := range tests {
+		exp := test.exp
+		res := test.res()
+		if !equals(exp, res) {
+			t.Errorf("expected %v, got %v", exp, res)
+		}
+	}
 }
